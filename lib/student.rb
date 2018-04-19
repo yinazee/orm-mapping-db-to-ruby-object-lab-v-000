b422f1f91f943fe6cf72b213af0ff365dfa087f7
Yina Zhu <yinagit@gmail.com>
2018-04-19 23:00:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,7 +52,17 @@ class Student
   end
 end
 
+def self.students_below_12th_grade
+sql = <<-SQL
+  SELECT COUNT(*)
+  FROM students
+  WHERE grade < 12;
+SQL
 
+DB[:conn].execute(sql).map do |row|
+  self.new_from_db(row)
+end
+end
 
   def save
     sql = <<-SQL
